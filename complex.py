b1cb039c7105fef69844c29220f27d45e6356f8b
gitadak <138775532+gitadak@users.noreply.github.com>
2023-07-27 15:41:36 +0530

    Add files via upload

---
+++ complex.py
@@ -0,0 +1,8 @@
+import math
+a=complex(input("Enter the value of a: "))
+b=complex(input("Enter the value of b: "))
+print("Addition=",a+b)
+print("Subtraction=",a-b)
+print("Multiplication=",a*b)
+print("Division=",a/b)
+
