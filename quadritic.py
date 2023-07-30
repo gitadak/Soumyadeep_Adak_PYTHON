93fa6d6d1286db7e330496e4fa2a40fc2c643da5
gitadak <138775532+gitadak@users.noreply.github.com>
2023-07-30 12:10:38 +0530

    Update quadritic.py

--- quadritic.py
+++ quadritic.py
@@ -3,6 +3,6 @@ a=float(input("Enter the coefficient of x^2:"))
 b=float(input("Enter the coefficient of x:"))
 c=float(input("Enter the constant term:"))
 d=b**2-4*a*c
-x=((-b)+cmath.sqrt(d))/2*a
-y=((-b)-cmath.sqrt(d))/2*a
+x=((-b)+cmath.sqrt(d))/(2*a)
+y=((-b)-cmath.sqrt(d))/(2*a)
 print("Roots are--\n",x,'\n',y)
