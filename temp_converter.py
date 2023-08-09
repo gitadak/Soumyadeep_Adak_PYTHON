5f28af478ec530b3188d51b3c0b1dd042d8e4999
gitadak <138775532+gitadak@users.noreply.github.com>
2023-08-09 18:50:10 +0530

    Update temp_converter.py

--- temp_converter.py
+++ temp_converter.py
@@ -1,12 +1,12 @@
 temp1=float(input("Enter the temperature: "))
 unit1=input("Enter the unit: ")
-if unit1=='C':
+if unit1=='C' or unit1=='c':
     temp2=9*temp1/5.0+32
     unit2='F'
-elif unit1=='F':
+elif unit1=='F' or unit1=='f':
     temp2=(temp1-32)*5/9.0
     unit2='C'
 else:
     print("Invalid input")
-    exit(0)
+    raise SystemExit
 print("Converted temperature: %.2f\nUnit:"%temp2,unit2)
