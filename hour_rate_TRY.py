7f7c698f05cc6f03783b57a1df4614b79d63624f
gitadak <138775532+gitadak@users.noreply.github.com>
2023-08-09 18:42:35 +0530

    Update hour_rate_TRY.py

--- hour_rate_TRY.py
+++ hour_rate_TRY.py
@@ -1,12 +1,12 @@
-#Hour-rate using exception handeling
+#Hour-rate using exception handling
 try:
     hour=int(input("Enter the working hours: "))
     rate=float(input("Enter the hourly rate: "))
-    if hour>40:
-        pay=hour*rate*1.5
-    else:
-        pay=hour*rate
-    print("Amount to be paid = %.2f"%pay)
 except ValueError:
     print("Error, please enter numeric input")
-
+    raise SystemExit
+if hour>40:
+    pay=hour*rate*1.5
+else:
+    pay=hour*rate
+print("Amount to be paid = %.2f"%pay)
