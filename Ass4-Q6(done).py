ecdba69fb518bacbae79f82c919c79abb19ae0a4
gitadak <138775532+gitadak@users.noreply.github.com>
2023-08-15 23:15:46 +0530

    Update Ass4-Q6(done).py

--- Ass4-Q6(done).py
+++ Ass4-Q6(done).py
@@ -1,16 +1,17 @@
 count=0
-s=0
+s=0.0
 while(True):
     a=input("Enter the number:")
     if a=='done':
         break
     try:
-        b=int(a)
+        b=float(a)
         count+=1
         s+=b
-    except ValueError:
-        print("Invalid input")
+    except ValueError as x:
+        print(x)
 try:
-    print("Total = %d\nCount = %d\nAverage = %.2f"%(s,count,s/count))
+    print("Total = %.2f\nCount = %d\nAverage = %.2f"%(s,count,s/count))
 except ZeroDivisionError:
     print("No numbers")
+
