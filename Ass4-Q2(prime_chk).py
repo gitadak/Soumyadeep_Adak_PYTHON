09b1ede5d30a5abf53f087255a89d9267fb64717
gitadak <138775532+gitadak@users.noreply.github.com>
2023-08-15 21:37:35 +0530

    Update Ass4-Q2(prime_chk).py

--- Ass4-Q2(prime_chk).py
+++ Ass4-Q2(prime_chk).py
@@ -1,11 +1,12 @@
 import math
 try:
     n=int(input("Enter the number: "))
+    if n<=0:
+        raise Exception("Input positive number")
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            print("%d is a composite number"%n)
+            raise SystemExit
+    print("%d is a prime number"%n)
 except ValueError:
     print("Invalid input")
-    raise SystemExit
-for i in range(2,int(math.sqrt(n))+1):
-    if n%i==0:
-        print("%d is a composite number"%n)
-        raise SystemExit
-print("%d is a prime number"%n)
