4944804808b8b96de9da0e7deef3a104580f527a
gitadak <138775532+gitadak@users.noreply.github.com>
2023-09-09 20:40:06 +0530

    Add files via upload

---
+++ Ass6_Q9.py
@@ -0,0 +1,32 @@
+queue=[]
+rev=[]
+n=int(input("Highest no. of elements in Queue: "))
+while True:
+    ch=int(input("1. Enqueue\n2. Dequeue\n3. Exit\nEnter your choice: "))
+    if ch==3:
+        break
+    elif ch==1:
+        element=input("Enter the element: ")
+        if len(queue)==n:
+            print("Queue full")
+        else:
+            queue.append(element)
+        print("Queue:")
+        for i in range(len(queue)-1,-1,-1):
+            print(queue[i])
+    elif ch==2:
+        try:
+            queue.reverse()
+            print("Deleted element:")
+            print(queue.pop())
+            queue.reverse()
+            print("Queue:")
+            if len(queue)==0:
+                print("Empty Queue")
+            else:
+                for i in range(len(queue)-1,-1,-1):
+                    print(queue[i])
+        except IndexError:
+            print("Empty Queue")
+    else:
+        print("Invalid Input")
