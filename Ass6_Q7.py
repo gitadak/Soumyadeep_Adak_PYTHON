17ae2157b8e8fb45b106c8ef74df88b8c4be1760
gitadak <138775532+gitadak@users.noreply.github.com>
2023-09-09 20:11:47 +0530

    Update Ass6_Q7.py

--- Ass6_Q7.py
+++ Ass6_Q7.py
@@ -1,10 +1,18 @@
 x=input("Enter the string: ")
-x=''.join(sorted(x))
-print(x)
 x=list(x)
 y=list(set(x))
 c=[]
-for i in range(len(y)):
-    c.append(x.count(y[i]))
-    print((y[i],c[i]))
 
+for i in range(65,91):
+    r=[]
+    if chr(i) in y:
+        r.extend([chr(i),x.count(chr(i))])
+        c.append(r)
+    r=[]
+    if chr(i+32) in y:
+        r.extend([chr(i+32),x.count(chr(i+32))])
+        c.append(r)
+        
+print("Frequency:")
+for i in range(len(c)):
+    print(c[i][0],"---->",c[i][1])
