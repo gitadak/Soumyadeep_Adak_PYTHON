c27addaec43f4a0a33bcb9720e2076cfe0b9b320
gitadak <138775532+gitadak@users.noreply.github.com>
2023-09-09 20:21:19 +0530

    Update Ass6_Q8.py

--- Ass6_Q8.py
+++ Ass6_Q8.py
@@ -1,14 +1,18 @@
 stack=[]
+n=int(input("Highest no. of elements in Stack: "))
 while True:
     ch=int(input("1. PUSH\n2. POP\n3. Exit\nEnter your choice: "))
     if ch==3:
         break
     elif ch==1:
         element=input("Enter the element: ")
-        stack.append(element)
-        print("Stack:")
-        for i in range(len(stack)-1,-1,-1):
-            print(stack[i])
+        if len(stack)==n:
+            print("Stack overflow")
+        else:
+            stack.append(element)
+            print("Stack:")
+            for i in range(len(stack)-1,-1,-1):
+                print(stack[i])
     else:
         try:
             print("Popped element:")
