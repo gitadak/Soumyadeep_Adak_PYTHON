f03d723cd97975c7b446f97c7a65a1be9c52a95d
gitadak <138775532+gitadak@users.noreply.github.com>
2023-09-12 21:09:23 +0530

    Update Ass6_Q2.py

--- Ass6_Q2.py
+++ Ass6_Q2.py
@@ -1,10 +1,9 @@
-print("Enter 10 integers:")
+n=int(input("Enter the no. of values: "))
 values=[]
-for i in range(10):
+for i in range(n):
     newValue=int(input("Enter integer %d:"%(i+1)))
     values.append(newValue)
-    print("Histogram:")
-    print("%s %10s %10s"%("Element","Value","Histogram"))
-    for i in range(len(values)):
-        print("%7d %10d %s"%(i,values[i],"*"*values[i]))
-print(values)
+print("Histogram:")
+print("%s %10s %10s"%("Element","Value","Histogram"))
+for i in range(len(values)):
+    print("%7d %10d  %s"%(i,values[i],"*"*values[i]))
