fb4536a5ad37653e5005d18755b6220ebd3a5df1
gitadak <138775532+gitadak@users.noreply.github.com>
2023-09-30 20:15:23 +0530

    Update Ass9_Q3.py

--- Ass9_Q3.py
+++ Ass9_Q3.py
@@ -1,20 +1,22 @@
+#LCM of n numbers
+def LCMofTwoNumbers(a,b):
+    c=a
+    i=2
+    while c%b!=0:
+        c=a*i
+        i+=1
+    return c
+
+def LCMofnNumbers(m):
+    LCM=LCMofTwoNumbers(m[0],m[1])
+    for i in range(2,len(m)):
+        LCM=LCMofTwoNumbers(LCM,m[i])
+    return LCM
+
 n=int(input("Enter the no. of elements: "))
 l=[]
 print("Enter the numbers:")
 for i in range(n):
     x = int(input())
     l.append(x)
-
-def lcm(n1,n2):
-    i=1
-    while i<=n1 and i<=n2:
-        if n1%i==0 and n2%i==0 :
-            k=i
-        i+=1
-    return (n1*n2)//k
-
-LCM=lcm(l[0],l[1])
-for i in range(2,len(l)):
-    LCM=lcm(LCM,l[i])
-
-print("LCM:",LCM)
+print("LCM = ",LCMofnNumbers(l))
