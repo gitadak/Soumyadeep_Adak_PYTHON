addcd9ab47e420640bd3a8b4400030059328ff13
gitadak <138775532+gitadak@users.noreply.github.com>
2023-10-01 13:11:59 +0530

    Add files via upload

---
+++ Ass10_Q8.py
@@ -0,0 +1,21 @@
+from animals_package import fish, birds, amphibians, mammals, reptiles
+
+# Example and characteristics of fish
+print(fish.example())
+print(fish.characteristics())
+
+# Example and characteristics of birds
+print(birds.example())
+print(birds.characteristics())
+
+# Example and characteristics of amphibians
+print(amphibians.example())
+print(amphibians.characteristics())
+
+# Example and characteristics of mammals
+print(mammals.example())
+print(mammals.characteristics())
+
+# Example and characteristics of reptiles
+print(reptiles.example())
+print(reptiles.characteristics())
